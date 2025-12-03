da615787e8169c40667c26fe32407e5514fae868
lggyx <19546328912@163.com>
2025-12-03 21:57:11 +0800

    系统配置管理业务代码开发

--- src/main/java/com/lggyx/service/impl/ContactServiceImpl.java
+++ src/main/java/com/lggyx/service/impl/ContactServiceImpl.java
@@ -1,5 +1,7 @@
 package com.lggyx.service.impl;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.lggyx.enumeration.SuccessCode;
 import com.lggyx.pojo.dto.ContactDTO;
 import com.lggyx.pojo.entity.Contact;
 import com.lggyx.mapper.ContactMapper;
@@ -7,6 +9,8 @@ import com.lggyx.pojo.vo.ContactVO;
 import com.lggyx.result.Result;
 import com.lggyx.service.IContactService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import jakarta.annotation.Resource;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 /**
@@ -19,14 +23,25 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ContactServiceImpl extends ServiceImpl<ContactMapper, Contact> implements IContactService {
+    @Resource
+    private ContactMapper contactMapper;
 
     @Override
     public Result<ContactVO> getContact() {
-        return null;
+        Contact contact = contactMapper.selectById(1);
+        ContactVO contactVO = new ContactVO();
+        BeanUtils.copyProperties(contact, contactVO);
+        return Result.success(SuccessCode.SUCCESS, contactVO);
     }
 
     @Override
     public Result<Void> updateContact(ContactDTO contactDTO) {
-        return null;
+        Contact contact = new Contact();
+        BeanUtils.copyProperties(contactDTO, contact);
+        int update = contactMapper.update(
+                contact,
+                Wrappers.<Contact>lambdaQuery().eq(Contact::getId, 1)
+        );
+        return update > 0 ? Result.success() : Result.error("更新失败");
     }
 }
